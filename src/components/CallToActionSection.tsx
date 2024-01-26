f10af69725eefc9ed5de5cc6a7bb9f814be6e66d
Dongming Shen <dongmingshen@icloud.com>
2024-01-26 16:48:06 +0800

    [F]call_to_action_2

--- src/components/CallToActionSection.tsx
+++ src/components/CallToActionSection.tsx
@@ -2,17 +2,18 @@ import React from "react";
 
 const CallToActionSection: React.FC = () => {
   return (
-    <div className="bg-yellow-300 px-36 py-24 flex justify-center items-center flex-col">
-      {/* Replace the image paths with the correct ones for your project */}
-
+    <div className="bg-yellow-300 px-64 py-28 flex justify-center items-center flex-col">
       <h2 className="text-4xl font-bold text-center mb-4">
         Compose your initial essay draft today.
       </h2>
 
-      <button className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded inline-flex items-center">
+      <a
+        href="/free"
+        className="bg-black hover:bg-gray-800 text-white font-bold py-2 px-4 rounded inline-flex items-center"
+      >
         <span>Start for Free</span>
         {/* You can include an SVG or icon here for the arrow */}
-      </button>
+      </a>
     </div>
   );
 };

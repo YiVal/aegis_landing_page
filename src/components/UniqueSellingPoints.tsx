78de6fd24b62a5e1f771963853a28264a0459c6b
Dongming Shen <dongmingshen@icloud.com>
2024-01-27 16:13:18 +0800

    [F]fix_stats

--- src/components/UniqueSellingPoints.tsx
+++ src/components/UniqueSellingPoints.tsx
@@ -21,9 +21,7 @@ const comparisonData = [
     bgColor: "bg-purple-200", // Tailwind CSS background color
   },
   {
-    titleComponent: (
-      <img src={GunnarSvg} alt="Gunnar" className="h-12 w-auto" />
-    ),
+    titleComponent: <img src={GunnarSvg} alt="Gunnar" className="h-8 w-auto" />,
     points: [
       "Charged $30 per single page, plus extra fees on tables and graphs",
       "Need to go back and forth with the writer on the topics, outlines, etc",
@@ -32,7 +30,7 @@ const comparisonData = [
     bgColor: "bg-red-200", // Tailwind CSS background color
   },
   {
-    titleComponent: <img src={GptSvg} alt="GPT" className="h-12 w-auto" />,
+    titleComponent: <img src={GptSvg} alt="GPT" className="h-8 w-auto" />,
     points: [
       "Unable to produce multiple paragraphs",
       "Unable to produce consistent content throughout each section of the essay",
@@ -44,32 +42,33 @@ const comparisonData = [
 
 const ComparisonSection: React.FC = () => {
   return (
-    <div className="px-36 py-12 max-w-7xl mx-auto flex flex-col justify-center">
-      <h2 className="text-3xl font-bold max-w-lg mx-auto text-gray-900 mb-8">
+    <div
+      id="selling-points"
+      className="px-36 py-12 mx-auto flex flex-col text-sm gap-12"
+    >
+      <h2 className="text-3xl text-center font-bold max-w-lg mx-auto text-gray-900">
         What makes our products unique compared to competitors?
       </h2>
-      <div className="flex flex-row">
-        {comparisonData.map((product, index) => (
+      <div className="flex gap-4 justify-center">
+        {comparisonData.map((product) => (
           <div
-            key={index}
-            className={`w-96 rounded-lg shadow-lg mx-4 flex flex-col justify-start ${product.bgColor}`}
+            key={product.titleComponent.toString()}
+            className={`rounded-lg flex flex-col ${product.bgColor}`}
           >
-            <div className="h-24 px-6 flex items-center justify-start text-sm">
+            <div className="h-16 px-4 flex items-center ">
               {product.titleComponent}
             </div>
-            <div className="bg-white p-4 h-72 rounded">
-              <ul className="flex flex-col">
-                {product.points.map((point) => (
-                  <li
-                    key={point}
-                    className="text-gray-600 text-sm flex flex-row items-start py-2"
-                  >
-                    <CheckmarkIcon className="w-4 h-4 mr-2 mt-0.5 inline-block" />
-                    <div className="w-52">{point}</div>
-                  </li>
-                ))}
-              </ul>
-            </div>
+            <ul className="flex flex-col bg-white p-4 h-72 rounded">
+              {product.points.map((point) => (
+                <li
+                  key={point}
+                  className="text-gray-600 flex flex-row items-start py-2 gap-1"
+                >
+                  <CheckmarkIcon className="w-4 h-4" />
+                  <div className="w-52">{point}</div>
+                </li>
+              ))}
+            </ul>
           </div>
         ))}
       </div>

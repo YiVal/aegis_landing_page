890674b42980d7dfdef201ed2d10676217022079
Dongming Shen <dongmingshen@icloud.com>
2024-01-27 13:47:23 +0800

    [F]fix_topbar

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -4,36 +4,27 @@ const TopBar: React.FC = () => {
   return (
     <nav
       id="topbar"
-      className="bg-white shadow-md mx-0 md:px-24 px-36 h-20 flex items-center justify-between  text-sm"
+      className="px-36 h-20 flex items-center justify-between text-sm"
     >
-      <a href="#topbar" className="flex flex-row justify-start h-8 w-auto">
-        <img src="/favicon.ico" alt="Logo" />
-        <span className="text-xl font-semibold">STEM Essay!</span>
+      <a href="#topbar" className="flex items-center">
+        <img className="w-8 h-8" src="/favicon.ico" alt="Logo" />
+        <div className="text-xl ml-1 font-extrabold">STEM Essay!</div>
       </a>
-      <div className="flex items-center justify-center">
-        <a
-          href="#pricing"
-          className="whitespace-nowrap text-gray-500 hover:text-gray-900 mx-5"
-        >
+      <div className="flex items-center">
+        <a href="#pricing" className=" hover:text-gray-900 mx-5">
           Pricing
         </a>
-        <a
-          href="#testimonial"
-          className="whitespace-nowrap text-gray-500 hover:text-gray-900 mx-5"
-        >
+        <a href="#testimonial" className=" hover:text-gray-900 mx-5">
           Testimonial
         </a>
       </div>
-      <div className="flex items-center justify-end">
-        <a
-          href="/login"
-          className="whitespace-nowrap inline-flex items-center justify-center text-gray-500 hover:text-gray-900 px-4 py-2"
-        >
+      <div className="flex items-center h-10">
+        <a href="/login" className=" text-gray-500 hover:text-gray-900 px-4">
           Log In
         </a>
         <a
           href="/start"
-          className="bg-blue-500 hover:bg-blue-700 text-white px-4 py-1 rounded-md"
+          className="bg-blue-500 h-full px-4 hover:bg-blue-700 text-white rounded-md flex items-center"
         >
           Start for Free
         </a>

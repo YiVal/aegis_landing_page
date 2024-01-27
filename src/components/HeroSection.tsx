1c035c0d7307a11a414a447c6331c17950a198f2
Dongming Shen <dongmingshen@icloud.com>
2024-01-27 14:28:22 +0800

    [F]fix_hero

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,39 +1,35 @@
 import React from "react";
 
-import stemEssayImage from "../assets/stem-essay.gif"; // Path to your GIF
-
 const HeroSection: React.FC = () => {
   return (
-    <div className="flex flex-col justify-center py-8">
-      <div className="bg-white p-0.5 inline-flex items-center space-x-2 rounded-full mx-auto ">
-        <span className="text-xs font-bold text-white px-2 py-1 bg-black rounded-full">
+    <div className="flex flex-col py-12 px-36 text-xs">
+      <div className="bg-white flex rounded-full mx-auto">
+        <div className="font-bold text-white p-1 bg-black rounded-full">
           New
-        </span>
-        <span className="text-xs font-medium text-black">
-          Guide to visually impressive essays! 🎉
-        </span>
+        </div>
+        <div className="p-1">Guide to visually impressive essays! 🎉</div>
       </div>
-      <div className="flex items-center text-center justify-center py-4">
-        <span className="text-5xl font-bold mb-4 max-w-3xl">
+      <div className="flex text-center justify-center my-8">
+        <div className="text-5xl font-bold max-w-3xl">
           5-Minute STEM Essays: Plagiarism-Free
-        </span>
+        </div>
       </div>
-      <div className="flex items-center text-center justify-center py-4">
-        <span className="text-sm text-gray-500 max-w-72">
+      <div className="flex text-center justify-center py-4">
+        <div className="text-sm text-gray-500 max-w-72">
           The easy way to write a well-researched and informative STEM essay
-        </span>
+        </div>
       </div>
-      <div className="flex justify-center py-4">
-        <a
-          href="/start"
-          className="bg-blue-600 text-white font-bold py-2 px-4 rounded hover:bg-blue-700"
-        >
-          Start for Free
-        </a>
-      </div>
-      <div className="flex justify-center p-8">
-        <img src={stemEssayImage} alt="StemEssay" className="mx-auto mb-8" />
-      </div>
+      <a
+        href="/start"
+        className="flex justify-center bg-blue-600 mx-auto text-white py-2 text-sm px-6 my-4 rounded hover:bg-blue-700"
+      >
+        Start for Free
+      </a>
+      <img
+        src="/images/stem-essay.gif"
+        alt="StemEssay"
+        className="mx-auto flex justify-center"
+      />
     </div>
   );
 };
--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -6,28 +6,30 @@ const TopBar: React.FC = () => {
       id="topbar"
       className="px-36 h-20 flex items-center justify-between text-sm"
     >
-      <a href="#topbar" className="flex items-center">
-        <img className="w-8 h-8" src="/favicon.ico" alt="Logo" />
-        <div className="text-xl ml-1 font-extrabold">STEM Essay!</div>
-      </a>
-      <div className="flex items-center">
+      <div className="flex justify-between items-center w-1/2">
+        <a href="#topbar" className="flex items-center">
+          <img className="w-8 h-8" src="/favicon.ico" alt="Logo" />
+          <div className="text-xl ml-1 font-extrabold">STEM Essay!</div>
+        </a>
         <a href="#pricing" className=" hover:text-gray-900 mx-5">
           Pricing
         </a>
+      </div>
+      <div className="flex justify-between items-center w-1/2">
         <a href="#testimonial" className=" hover:text-gray-900 mx-5">
           Testimonial
         </a>
-      </div>
-      <div className="flex items-center h-10">
-        <a href="/login" className=" text-gray-500 hover:text-gray-900 px-4">
-          Log In
-        </a>
-        <a
-          href="/start"
-          className="bg-blue-500 h-full px-4 hover:bg-blue-700 text-white rounded-md flex items-center"
-        >
-          Start for Free
-        </a>
+        <div className="flex justify-between items-center h-10">
+          <a href="/login" className=" text-gray-500 hover:text-gray-900 px-4">
+            Log In
+          </a>
+          <a
+            href="/start"
+            className="bg-blue-500 h-full px-4 hover:bg-blue-700 text-white rounded-md flex items-center"
+          >
+            Start for Free
+          </a>
+        </div>
       </div>
     </nav>
   );

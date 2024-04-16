1ae6002f5fd90ef04f88572dc0605c8ba13edca6
Shuya Yang <shuyayang@Shuyas-MacBook-Pro.local>
2024-04-16 13:24:33 -0400

    add calender

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 import { ReactComponent as BackgroundImage } from "../assets/3D Morph Lines 12.svg";
 
+// Declare Calendly as a global variable
+declare global {
+  interface Window {
+    Calendly: any;
+  }
+}
+
 const HeroSection: React.FC = () => {
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const link = document.createElement("link");
+    link.href = "https://assets.calendly.com/assets/external/widget.css";
+    link.rel = "stylesheet";
+    document.head.appendChild(link);
+
+    const script = document.createElement("script");
+    script.src = "https://assets.calendly.com/assets/external/widget.js";
+    script.async = true;
+    document.body.appendChild(script);
+
+    return () => {
+      document.head.removeChild(link);
+      document.body.removeChild(script);
+    };
+  }, []);
 
   return (
     <div
@@ -32,19 +56,28 @@ const HeroSection: React.FC = () => {
           bottom: 0,
           left: 0,
           width: "100%",
-          height: "408px", // set to original SVG height or use a percentage
+          height: "408px",
           zIndex: 1,
         }}
       >
         <BackgroundImage style={{ width: "100%", height: "100%" }} />
       </div>
-      <button
-        onClick={() => navigate("/demo")}
+      {/* Calendly link widget begin */}
+      <a
+        href="#"
+        onClick={(e) => {
+          e.preventDefault();
+          window.Calendly.initPopupWidget({
+            url: "https://calendly.com/luhan-szyj/30min",
+          });
+          return false;
+        }}
         className="flex justify-center bg-white mt-8 mx-auto text-bgblue py-2 text-sm px-6 my-4 rounded hover:bg-blue-700 hover:text-white"
         style={{ position: "relative", zIndex: 10 }}
       >
         Book a demo
-      </button>
+      </a>
+      {/* Calendly link widget end */}
     </div>
   );
 };
